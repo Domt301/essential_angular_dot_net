cefc3c9da3e08de9810fc3ea59f186f1a5eb5fb0
Domt301 <leonardod.trimarchi@gmail.com>
2017-10-26 10:36:26 -0400

    added navigation to different views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,5 +7,16 @@ namespace SportsStore.Controllers {
             ViewBag.Message = "Sports Store App";
             return View();
         }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is an about page with Angular and netcore Mvc";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
